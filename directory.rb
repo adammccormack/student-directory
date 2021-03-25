69a2aeac94c73d382f6b0de3a2cab74405290891
Adam <adam.j.mccor@gmail.com>
2021-03-25 18:09:30 +0000

    Refactored directory.rb

--- directory.rb
+++ directory.rb
@@ -1,74 +1,62 @@
-#let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+@students = []
 
 def interactive_menu
-  students = []
   loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      #input the students
-      when "1"
-        students = input_students
-      when "2"
-        # show the students
-        print_header
-        print(students)
-        print_footer
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
-  end
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
 end
 
 def input_students
   puts "Please enter the names of the students"
-  puts "to finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
+  puts "To finish, just hit return twice"
+  @students = [] 
   name = gets.chomp
-  # while the name is not empty, repeat this codee
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  # return the array of students
 end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(students)
+
+def print_students_list(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
-#nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+interactive_menu
